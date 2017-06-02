958da41b47f0fbf2861184950169950cbc2bcc4d
Saurabh Chandra Bose <Saurabh@Saurabh-Chandra-Boses-MacBook-Pro.local>
2017-06-02 11:45:53 +0530

    fully developed 2 web pages

---
+++ Modernist/script/myScript.js
@@ -0,0 +1,120 @@
+$(document).ready(function() {
+    
+    
+    document.getElementById("date").innerHTML=Date();
+    
+    var currentIndex = 0;
+    var items = $(".slide-container > div");
+    var itemAmt = items.length;
+    
+    function cycleItems() {
+        var item = $(".slide-container > div").eq(currentIndex);
+        items.hide();
+        item.css("display", "inline-block");
+    }
+    
+    var autoSlide = setInterval(function() {
+        currentIndex += 1;
+        if (currentIndex > itemAmt - 1) {
+            currentIndex = 0;
+        }
+        cycleItems();
+    }, 1000);
+    
+    
+    $(".next").click(function() {
+        clearInterval(autoSlide);
+        
+        
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+      var currentIndex = 0,
+      items = $('.slide-container > div'),
+      itemAmt = items.length;
+
+    function cycleItems() {
+      var item = $('.slide-container > div').eq(currentIndex);
+      items.hide();
+      item.css('display','inline-block');
+    }
+
+    var autoSlide = setInterval(function() {
+      currentIndex += 1;
+      if (currentIndex > itemAmt - 1) {
+        currentIndex = 0;
+      }
+      cycleItems();
+    }, 1000);
+
+    $('.next').click(function() {
+      clearInterval(autoSlide);
+      currentIndex += 1;
+      if (currentIndex > itemAmt - 1) {
+        currentIndex = 0;
+      }
+      cycleItems();
+    });
+
+    $('.prev').click(function() {
+      clearInterval(autoSlide);
+      currentIndex -= 1;
+      if (currentIndex < 0) {
+        currentIndex = itemAmt - 1;
+      }
+      cycleItems();
+    });
+    
+    */
+    
+    
+});
+
+
+
+
+
+
+
+/*var currentIndex = 0,
+  items = $('.container div'),
+  itemAmt = items.length;
+
+function cycleItems() {
+  var item = $('.container div').eq(currentIndex);
+  items.hide();
+  item.css('display','inline-block');
+}
+
+var autoSlide = setInterval(function() {
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+}, 3000);
+
+$('.next').click(function() {
+  clearInterval(autoSlide);
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+});
+
+$('.prev').click(function() {
+  clearInterval(autoSlide);
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = itemAmt - 1;
+  }
+  cycleItems();
+});*/
